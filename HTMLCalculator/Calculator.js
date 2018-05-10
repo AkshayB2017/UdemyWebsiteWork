8987d9236da6f8a3390ff368c287ec034255ad16
Akshay Balakrishnan <akbgunner4ever@gmail.com>
2018-05-10 19:58:19 +0530

    Added listening module for buttons in JS file

---
+++ HTMLCalculator/Calculator.js
@@ -0,0 +1,31 @@
+const calculator=document.querySelector('.calculator')
+const keys=calculator.querySelector('.calculator__key')
+keys.addEventListener('click',e =>
+{ if(e.target.matches('button')){//Do something
+ }
+
+})
+
+const key=e.target
+const action=key.dataset.action
+if(!action)
+{
+     console.log('number key!')
+}
+
+if(action=='add'|| action=='subtract' || action=='multiply' || action== 'divide'){
+    console.log('operator key!')
+
+}
+if(action=='decimal'){
+    console.log('decimal key!')
+
+}
+if(action=='clear'){
+    console.log('clear key!')
+    
+}
+if(action=='calculate'){
+    console.log('equal key!')
+    
+}
