f69431a7703dff40a09578ec6f4431d35b78d043
Akshay Balakrishnan <akbgunner4ever@gmail.com>
2018-05-11 09:58:40 +0530

    Modified JS file

--- HTMLCalculator/Calculator.js
+++ HTMLCalculator/Calculator.js
@@ -1,7 +1,15 @@
-const calculator=document.querySelector('.calculator')
-const keys=calculator.querySelector('.calculator__key')
+console.log('hello')
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+
 keys.addEventListener('click',e =>
-{ if(e.target.matches('button')){//Do something
+{ if(e.target.matches('button')){
+    const key=e.target
+  const action=key.dataset.action
+  const keyContent= key.textContent
+  const displayedNum=display.textContent()
+  Array.from(key.parentNode.children)
+     .forEach(k=>k.classList.remove('is-depressed'))
  }
 
 })
@@ -29,3 +37,30 @@ if(action=='calculate'){
     console.log('equal key!')
     
 }
+
+const display=document.querySelector('.calculator__display')
+
+
+if(!action)
+{
+    if(displayedNum=='0')
+    { display.textContent=keyContent
+
+    }
+    else{
+        display.textContent=displayedNum+keyContent
+
+    }
+    if(action=='decimal')
+    {
+        display.textContent=displayedNum+'.'
+
+    }
+    if(action=='add'||action=='subtract'|| action=='multiply'|| action=='divide')
+    {
+        key.classList.add('is-depressed')
+
+    }
+
+}
+
